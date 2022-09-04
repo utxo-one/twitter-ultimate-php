f0c0788d176a2124d82c14f60607d97338f6cace
utxo.one <github@utxo.one>
2022-09-03 22:25:27 -0400

    gitignore

--- src/Clients/ListClient.php
+++ src/Clients/ListClient.php
@@ -4,7 +4,8 @@ namespace UtxoOne\TwitterUltimatePhp\Clients;
 
 use UtxoOne\TwitterUltimatePhp\Models\Tweets;
 use UtxoOne\TwitterUltimatePhp\Models\TwitterList;
-use UtxoOne\TwitterUltimatePhp\Models\User;
+use UtxoOne\TwitterUltimatePhp\Models\TwitterLists;
+use UtxoOne\TwitterUltimatePhp\Models\Users;
 
 class ListClient extends BaseClient
 {
@@ -17,13 +18,13 @@ class ListClient extends BaseClient
         return new TwitterList($response->getData());
     }
 
-    public function getUserOwnedLists(string $id): User
+    public function getUserOwnedLists(string $id): TwitterLists
     {
         $response = $this->get('users/' . $id . '/owned_lists', [
             'list.fields' => $this->listFields,
         ]);
 
-        return new User($response->getData());
+        return new TwitterLists($response->getData());
     }
 
     public function getListTweets(string $id): Tweets
@@ -33,5 +34,50 @@ class ListClient extends BaseClient
         ]);
 
         return new Tweets($response->getData());
+    }
+
+    public function getListMembers(string $id): Users
+    {
+        $response = $this->get('lists/' . $id . '/members', [
+            'user.fields' => $this->userFields,
+        ]);
+
+        return new Users($response->getData());
+    }
+
+    public function getUserMemberships(string $id): TwitterLists
+    {
+        $response = $this->get('users/' . $id . '/lists_memberships', [
+            'list.fields' => $this->listFields,
+        ]);
+
+        return new TwitterLists($response->getData());
+    }
+
+    public function getListFollowers(string $id): Users
+    {
+        $response = $this->get('lists/' . $id . '/followers', [
+            'user.fields' => $this->userFields,
+        ]);
+
+        return new Users($response->getData());
+    }
+
+    public function getUserFollowedLists(string $id): TwitterLists
+    {
+        $response = $this->get('users/' . $id . '/followed_lists', [
+            'list.fields' => $this->listFields,
+        ]);
+
+        return new TwitterLists($response->getData());
+    }
+
+    public function getUserPinnedLists(string $id): TwitterLists
+    {
+        $response = $this->get('users/' . $id . '/pinned_lists', [
+            'list.fields' => $this->listFields,
+        ]);
+
+        return new TwitterLists($response->getData());
     }
 }
